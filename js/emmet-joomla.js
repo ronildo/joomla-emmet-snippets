07b94b01fc8421a3824f92fb6c49c6cf19937afd
Ronildo Costa <ronildo@gmail.com>
2013-01-07 16:56:01 -0200

    Adding content to the page reading the json

--- js/emmet-joomla.js
+++ js/emmet-joomla.js
@@ -1,8 +1,27 @@
 var url = 'js/emmet-joomla.json';
 
 $.getJSON(url, function(data) {
-  $.each(data.html.snippets, function(i){
-    var li = $('<li>').text(this);
-    $('#joomla').append(li);
+  
+  console.log(data);
+
+  var abbreviations = [], i = 0;
+  $.each(data.html.abbreviations, function(m, n){
+    abbreviations.push(m) 
   });
+
+  // function formatString(string){
+  //   return string.replace("\n","<br />"); 
+  // }
+
+  $.each(data.html.snippets, function(k, e){
+    // var command = formatString(e);
+    var li = $('<li>').addClass('item');
+    var div1 = $('<span>').text(abbreviations[i]).addClass('abbreviation');
+    var div2 = $('<span>').text(e).addClass('command');
+    li.append(div1).append(div2);
+    $('#items').append(li);
+    i++;
+  });
+
 });
+
